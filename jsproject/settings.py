6e5cbee6b46ef8bc93716b9be56c4f3528d22e90
usamaru123 <usam260@gmail.com>
2024-02-13 01:43:19 +0900

    static

--- jsproject/settings.py
+++ jsproject/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 #TATICFILES_DIRS = [
 #    os.path.join(BASE_DIR,'static/')
