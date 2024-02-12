c4fd7e1d2307350d4bf87d3006155a821a200fbb
usamaru123 <usam260@gmail.com>
2024-02-13 01:05:45 +0900

    g

--- jsproject/settings.py
+++ jsproject/settings.py
@@ -129,7 +129,7 @@ STATIC_URL = '/static/'
 STATIC_DIR = Path.joinpath(BASE_DIR,'static')
 
 STATICFILES_DIRS = [
-     os.path.join(BASE_DIR,'static')
+     os.path.join(BASE_DIR,'static/')
 ]
 
 # Default primary key field type
