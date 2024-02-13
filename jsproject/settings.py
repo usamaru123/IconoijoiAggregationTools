f219c94894d5fb772f2d96d692d7a330cb95996d
usamaru123 <usam260@gmail.com>
2024-02-13 20:10:34 +0900

    settings.py

--- jsproject/settings.py
+++ jsproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-h8mxlj(695m3$^f1rebv_47a6@=%w33$cpt1c9$i-02fhj*nu!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['162.43.30.152','www.ico-graph.com','127.0.0.1']
+ALLOWED_HOSTS = ['162.43.30.152','ico-graph.com','127.0.0.1']
 
 
 # Application definition
