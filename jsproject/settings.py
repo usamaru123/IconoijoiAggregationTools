09fc995ad5e26b1bf84e421b88a96a6c88c970a3
usamaru123 <usam260@gmail.com>
2024-02-13 19:31:59 +0900

    fff

--- jsproject/settings.py
+++ jsproject/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-h8mxlj(695m3$^f1rebv_47a6@=%w33$cpt1c9$i-02fhj*nu!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['162.43.30.152','www.ico-graph.com','localhost']
+ALLOWED_HOSTS = ['162.43.30.152','www.ico-graph.com','127.0.0.1']
 
 
 # Application definition
@@ -124,11 +124,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
+#STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
-#TATICFILES_DIRS = [
-#    os.path.join(BASE_DIR,'static/')
-#]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static/')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
