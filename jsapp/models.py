fd98f11d3d38b989bdb93a739cd42103ad963cac
usamaru123 <usam260@gmail.com>
2024-02-18 00:40:33 +0900

    f

--- jsapp/models.py
+++ jsapp/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.utils import timezone
 # Create your models here.
 class MenberModel(models.Model):
-   time = models.TimeField(default=timezone.now())
+   time = models.DateTimeField(default=timezone.now())
    title = models.CharField(max_length=100,blank=True)
    venue = models.CharField(max_length=100,blank=True)
 
