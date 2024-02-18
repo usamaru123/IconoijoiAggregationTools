680435a78dc157ea180244054e8169ea6efb21cc
usamaru123 <usam260@gmail.com>
2024-02-18 17:30:17 +0900

    f

--- jsapp/urls.py
+++ jsapp/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('main/',index,name='index'),
     path('ikolove/2024/tour/2/',AnswerCreate.as_view(),name='create'),
     path('eventcreate',EventCreate.as_view(),name='eventcreate'),
-    path('eventlist',EventList.as_view(),name='eventList'),
+    path('eventlist',EventList.as_view(),name='eventlist'),
 ]
