431946b3cb25a400828a3e236c6eb2ec11162a28
usamaru123 <usam260@gmail.com>
2024-02-18 16:53:16 +0900

    f

--- jsapp/urls.py
+++ jsapp/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import index,AnswerCreate,EventCreate,Eventlist
+from .views import index,AnswerCreate,EventCreate,EventList
 
 urlpatterns = [
     path('main/',index,name='index'),
     path('ikolove/2024/tour/2/',AnswerCreate.as_view(),name='create'),
     path('eventcreate',EventCreate.as_view(),name='eventcreate'),
-    path('eventlist',Eventlist.as_view(),name='eventlist'),
+    path('eventlist',EventList.as_view(),name='eventList'),
 ]
