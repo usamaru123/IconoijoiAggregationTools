6b5152df86f50a8630f1ddc28ba6a6d540a13969
usamaru123 <usam260@gmail.com>
2024-02-19 00:20:54 +0900

    foregin

--- jsapp/admin.py
+++ jsapp/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import MenberModel,EventModel
+from .models import MenberModel,EventModel,VenueModel
 # Register your models here.
 admin.site.register(MenberModel)
 admin.site.register(EventModel)
+admin.site.registar(VenueModel)
