1da259e8c1f1d583ef1af061067524f83e304d31
usamaru123 <usam260@gmail.com>
2024-02-19 11:19:01 +0900

    fl;flflflff;

--- jsapp/views.py
+++ jsapp/views.py
@@ -18,7 +18,7 @@ class AnswerCreate(CreateView):
     template_name = 'create2.html'
     model = MenberModel
 
-    fields = ('eventtitle','venue','matinee','evening','ticket1','sheet1','floor1','block1','number1','ticket2','sheet2','floor2','block2','number2',)
+    fields = ('eventtitle','venue','venueid','matinee','evening','ticket1','sheet1','floor1','block1','number1','ticket2','sheet2','floor2','block2','number2',)
     def get_context_data(self,*args,**kwargs,):
         ctx = super().get_context_data(**kwargs)
         ctx['title'] = VenueModel.objects.get(venueid=self.kwargs['num'])
