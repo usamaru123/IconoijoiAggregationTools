73d65382d623e5d3e2dc34a762b2189bc5881f1c
usamaru123 <usam260@gmail.com>
2024-02-19 09:44:18 +0900

    all()

--- jsapp/views.py
+++ jsapp/views.py
@@ -54,5 +54,5 @@ class VenueList(ListView):
     template_name = 'venuelist.html'
     def get_context_data(self,**kwargs):
         ctx = super().get_context_data(**kwargs)
-        ctx['venue'] = VenueModel.objects.order_by('venuedate')
+        ctx['venue'] = VenueModel.objects.all()
         return ctx
