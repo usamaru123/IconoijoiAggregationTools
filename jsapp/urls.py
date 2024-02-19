b9a6295bece7bbf72200c3062c24668f1bd81b5c
usamaru123 <usam260@gmail.com>
2024-02-19 10:52:30 +0900

    AnswerList

--- jsapp/urls.py
+++ jsapp/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import index,AnswerCreate,EventCreate,EventList,VenueCreate,VenueList
+from .views import AnswerList,AnswerCreate,EventCreate,EventList,VenueCreate,VenueList
 
 urlpatterns = [
-    path('main/',index,name='index'),
-    path('ikolove/<int:num>/',AnswerCreate.as_view(),name='create'),
+    path('result/<int:num>',AnswerList.as_view(),name='index'),
+    path('<int:num>/',AnswerCreate.as_view(),name='create'),
     path('eventcreate',EventCreate.as_view(),name='eventcreate'),
     path('eventlist',EventList.as_view(),name='eventlist'),
     path('venuecreate',VenueCreate.as_view(),name='venuecreate'),
--- jsapp/views.py
+++ jsapp/views.py
@@ -4,26 +4,22 @@ from .models import MenberModel,EventModel,VenueModel
 from django.urls import reverse_lazy
 from django.http import HttpResponse
 
-def index(request):
-    resultsTitle = '＝LOVEアリーナツアー2024 「Tell me what\'s more than \"LOVE\"」'
-    results = MenberModel.objects.all()
-    context = {
-        'results':results,
-    }   
-    return render(request,'index.html',context)
-# Create your views here.
 
 
+class AnswerList(ListView):
+    template_name = 'index.html'
+    model = MenberModel
+    def get_context_data(self,*args,**kwargs,):
+        ctx = super().get_context_data(**kwargs)
+        ctx['title'] = VenueModel.objects.get(venueid=self.kwargs['num'])
+        return  ctx
 
 class AnswerCreate(CreateView):
     template_name = 'create2.html'
     model = MenberModel
 
     fields = ('eventtitle','venue','matinee','evening','ticket1','sheet1','floor1','block1','number1','ticket2','sheet2','floor2','block2','number2',)
-    def get_context_data(self,*args,**kwargs,):
-        ctx = super().get_context_data(**kwargs)
-        ctx['title'] = VenueModel.objects.get(venueid=self.kwargs['num'])
-        return  ctx
+
 
     success_url = reverse_lazy('index')
 
