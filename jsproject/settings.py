baf590973d8451b265483c5d9dbd6d847ab5457b
usamaru123 <usam260@gmail.com>
2024-02-24 18:11:28 +0900

    ff

--- jsproject/settings.py
+++ jsproject/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'jsproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR,'templates'],
+        'DIRS': [BASE_DIR,'jsapp/templates'],
         'APP_DIRS': False,
         'OPTIONS': {
             'context_processors': [
