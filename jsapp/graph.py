afe08395f3e54ab463c6d931642ef5df08045d8a
usamaru123 <usam260@gmail.com>
2024-02-25 23:33:34 +0900

    ff

--- jsapp/graph.py
+++ jsapp/graph.py
@@ -111,7 +111,7 @@ def Arena_HeatMap(rows,columns,sheets):
         for j in alphabets:
             d = {'a': ippanlist[i][j], 'b': kamekolist[i][j], 'c': joseilist[i][j], 'd': chakusekilist[i][j]}
             points[i][j] = max(d.values())
-            text[j][i] = max(d,key=d.get)
+            text[i][j] = max(d,key=d.get)
 
     sheetdf = pd.DataFrame(points)
     textdf = pd.DataFrame(text)
