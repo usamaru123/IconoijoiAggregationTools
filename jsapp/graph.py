554433bbafd09a172d8cf9d9b118eca091450a07
usamaru123 <usam260@gmail.com>
2024-02-26 00:14:14 +0900

    ff

--- jsapp/graph.py
+++ jsapp/graph.py
@@ -140,7 +140,7 @@ def Arena_HeatMap(rows,columns,sheets):
     plt.yticks(rotation=0)
     plt.legend(prop={'family':'MS Gothic'})
     sns.set(font='MS Gothic')
-    sns.title('tofu - 豆腐f')
+    plt.title("Height vs Weight")
     graph = Output_Graph()
     return graph
 
