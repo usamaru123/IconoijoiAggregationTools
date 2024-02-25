57c98bd52112fb713687e148944927728c474a9d
usamaru123 <usam260@gmail.com>
2024-02-25 23:20:21 +0900

    ff

--- jsapp/graph.py
+++ jsapp/graph.py
@@ -109,7 +109,7 @@ def Arena_HeatMap(rows,columns,sheets):
     for i in range(1,len(blocklist)+1):
         for j in alphabets:
             d = {'a': ippanlist[i][j], 'b': kamekolist[i][j], 'c': joseilist[i][j], 'd': chakusekilist[i][j]}
-            points[i][j] = (ippanlist[i][j]+kamekolist[i][j]+joseilist[i][j]+chakusekilist[i][j])
+            points[i][j] = max(d.values())
 
     sheetdf = pd.DataFrame(points)
     sns.heatmap(sheetdf,square=True,cbar=False,cmap='nipy_spectral_r',linewidths=0.5,vmax=5.0,vmin=0,annot=True)
