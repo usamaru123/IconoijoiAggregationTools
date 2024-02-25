59747065ca8fa75c518a951a6f376d2410fb578a
usamaru123 <usam260@gmail.com>
2024-02-25 12:31:22 +0900

    ff

--- jsapp/models.py
+++ jsapp/models.py
@@ -9,6 +9,13 @@ class EventModel(models.Model):
 
    def __str__(self):
       return self.eventtitle
+   
+class HallInfoModel(models.Model):
+   hallid = models.IntegerField(primary_key=True)
+   hallname = models.CharField(max_length=100)
+   hallprefecture = models.CharField(max_length=100)
+   
+      
 
 class VenueModel(models.Model):
    venueid = models.IntegerField(primary_key=True)
@@ -40,6 +47,3 @@ class MenberModel(models.Model):
    block2 = models.CharField(max_length=100,blank=True)
    number2 = models.CharField(max_length=100,blank=True)
 
-class HallInfoModel(models.Model):
-   hallid = models.IntegerField(primary_key=True)
-      
