41f0e3ef5edda95455fafbc44ea95ec39a7ef630
usamaru123 <usam260@gmail.com>
2024-02-25 12:21:50 +0900

    ff

--- jsapp/admin.py
+++ jsapp/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from .models import MenberModel,EventModel,VenueModel,HallInfoModel
+from .models import MenberModel,EventModel,VenueModel
 # Register your models here.
 admin.site.register(MenberModel)
 admin.site.register(EventModel)
 admin.site.register(VenueModel)
-admin.site.register(HallInfoModel)
