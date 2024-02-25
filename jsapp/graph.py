985447ac1dd383aab1012be16acb12a0f7405852
usamaru123 <usam260@gmail.com>
2024-02-25 16:19:33 +0900

    ff

--- jsapp/graph.py
+++ jsapp/graph.py
@@ -59,6 +59,6 @@ def HeatMap(rows,numbers,sheets):
             number = int_numbers[s]
             sheetlist[row][number] = int_sheets[s]
     
-    sns.heatmap(sheetlist,square=True,cbar=False,cmap="hsv",linewidths=0.01)
+    sns.heatmap(sheetlist,square=True,cbar=False,cmap="gist_rainbow",linewidths=0.01)
     graph = Output_Graph()
     return graph
