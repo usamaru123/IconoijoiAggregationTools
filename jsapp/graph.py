4de3bf5a3380ac8be151817b10b75be7b875f188
usamaru123 <usam260@gmail.com>
2024-02-25 18:13:18 +0900

    ff

--- jsapp/graph.py
+++ jsapp/graph.py
@@ -68,7 +68,7 @@ def Arena_HeatMap(rows,columns,sheets):
 
     sheetlist = {}
     for s in range(len(sheets)):
-        if (sheets[s] != 0)and(columns[s] != 0)and(row[s] != 0):
+        if (sheets[s] != 0)and(columns[s] != 0)and(rows[s] != 0):
             row = rows[s]
             column = columns[s]
             sheetlist[row[s]+column[s]] = sheets[s]
