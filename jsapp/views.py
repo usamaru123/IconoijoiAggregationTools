07bd41734b6a7317205424dbdd30ac521c4e7681
usamaru123 <usam260@gmail.com>
2024-02-26 10:57:04 +0900

    ff

--- jsapp/views.py
+++ jsapp/views.py
@@ -15,9 +15,9 @@ class AnswerList(ListView):
 
     def get_context_data(self,*args,**kwargs,):
         ctx = super().get_context_data(**kwargs) 
-        row1 = [row.block_r1 for row in qs]
-        column1 = [number.block_c1 for number in qs]
-        sheet1 = [sheet.sheet1 for sheet in qs ]
+        #row1 = [row.block_r1 for row in qs]
+       # column1 = [number.block_c1 for number in qs]
+       # sheet1 = [sheet.sheet1 for sheet in qs ]
        # chart1 = graph.Arena_HeatMap(row1,column1,sheet1)
        # sheetratio1 = graph.sheetratio(sheet1)
         query = self.request.GET.get('query')
@@ -28,7 +28,7 @@ class AnswerList(ListView):
 
        # ctx['chart1'] = chart1
        # ctx['sheetratio1'] = sheetratio1
-        ctx['results'] = MenberModel.objects.filter(venueid=self.kwargs['num']).all()
+        ctx['results'] = qs
         ctx['title'] = VenueModel.objects.get(venueid=self.kwargs['num'])
         return  ctx
 
