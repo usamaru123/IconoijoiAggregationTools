a3667c8fd33e2c1f5195690693ca4c010d322be9
usamaru123 <usam260@gmail.com>
2024-02-26 11:31:23 +0900

    f

--- jsapp/views.py
+++ jsapp/views.py
@@ -22,7 +22,7 @@ class AnswerList(ListView):
        # sheetratio1 = graph.sheetratio(sheet1)
         evening = self.request.GET.get('evening')
         if evening:
-            qs = MenberModel.objects.values_list("ticket1",flat=True)
+            qs = MenberModel.objects.values('evening','ticket1','sheet1','floor1','row1','block_r1')
         else:
             qs = MenberModel.objects.filter(venueid=self.kwargs['num']).all()
 
