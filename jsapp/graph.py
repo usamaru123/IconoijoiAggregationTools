ae9bb7fccd7878f4020d4efa7959e8fc1f591517
usamaru123 <usam260@gmail.com>
2024-02-27 14:17:00 +0900

    f

--- jsapp/graph.py
+++ jsapp/graph.py
@@ -124,6 +124,8 @@ def Arena_HeatMap(rows,columns,sheets):
         y=sheetdf.index,
         z=np.array(sheetdf),
         colorscale='Spectral',
+        cmax = 2
+        cmin = -2        
         ))
     graph = fig.to_html(include_plotlyjs=False)
     return graph
