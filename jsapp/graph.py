82c33ebccb931d5ebac37c8179ed3658eea0614e
usamaru123 <usam260@gmail.com>
2024-02-27 12:39:59 +0900

    ff

--- jsapp/graph.py
+++ jsapp/graph.py
@@ -95,7 +95,7 @@ def Arena_HeatMap(rows,columns,sheets):
                 chakuseki += 1
 
     #すべてのリストでフィールドを参照し，一番集計数が多い座席種別をblocksheetに代入する
-    for int_column in range(len(int_columns)):
+    for int_column in int_columns:
         for int_row in int_rows:
             comparesheet = {'一':ippan,'カ':kameko,'女':josei,'着':chakuseki}
             maxsheet = max(comparesheet.values())
