a9a4731534a00287d4bb46610fab943f171f76f1
usamaru123 <usam260@gmail.com>
2024-02-27 10:33:29 +0900

    ff

--- jsapp/views.py
+++ jsapp/views.py
@@ -16,11 +16,11 @@ class AnswerList(ListView):
     def get_context_data(self,*args,**kwargs,):
         ctx = super().get_context_data(**kwargs)
         qs = MenberModel.objects.filter(venueid=self.kwargs['num']).all()
-        row1 = [row.block_r1 for row in qs]
-        column1 = [number.block_c1 for number in qs]
-        sheet1 = [sheet.sheet1 for sheet in qs ]
-        chart = graph.sheetratio(sheet1)
-        heatmap = graph.HeatMap(sheet1)
+        row = [row.block_r1 for row in qs]
+        column = [number.block_c1 for number in qs]
+        sheet = [sheet.sheet1 for sheet in qs ]
+        chart = graph.sheetratio(sheet)
+        heatmap = graph.HeatMap(row,column,sheet)
         evening = self.request.GET.get('evening')
 
         
