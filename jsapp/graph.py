57a8208e6abcee8093ec3bd5f5363caba2a94192
usamaru123 <usam260@gmail.com>
2024-02-27 15:35:30 +0900

    ff

--- jsapp/graph.py
+++ jsapp/graph.py
@@ -163,7 +163,7 @@ def sheetratio(sheets):
             xanchor='center',
             yanchor='bottom',
             orientation='h',
-            y=0.9
+            y=-0.2
         ),
         margin_l=0,
         margin_r=0
