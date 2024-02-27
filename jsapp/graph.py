a8c72d4ebdfa7d39567d8f98ff1c71f749eabecb
usamaru123 <usam260@gmail.com>
2024-02-27 16:47:24 +0900

    gg

--- jsapp/graph.py
+++ jsapp/graph.py
@@ -127,7 +127,6 @@ def Arena_HeatMap(rows,columns,sheets):
         x=sheetdf.columns,
         y=sheetdf.index,
         z=np.array(sheetdf),
-        annotation_text = np.array(textdf),
         colorscale='Spectral',
         zmax = 2,
         zmin = -2, 
