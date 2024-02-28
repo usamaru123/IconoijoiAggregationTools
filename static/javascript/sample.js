03192b1a77a40e14708901897fe7e0fe5c80af50
usamaru123 <usam260@gmail.com>
2024-02-29 00:25:25 +0900

    ff

--- static/javascript/sample.js
+++ static/javascript/sample.js
@@ -59,7 +59,7 @@ function positionformfunc(i) {
             <input pattern="[A-Ga-g]{1}" class="col-3 block position" id="block_r${i}" placeholder="英字1文字"  oninput="inputChange(${i})">
             <input type="number" min="1" max="9" class="col-3 number position" id="block_c${i}" name="block_c${i}" placeholder="半角数字">
         <b style="font-size:1.5rem">ブロック</b>      
-        </div><div id="errorform1_4">ff</div>
+        </div><div id="errorform${i}_4">ff</div>
         <div class="numberform">
             <input type="number" min="1" max="1000" class="col-3 number position" id="number${i}" name="number${i}"
             placeholder="半角数字">
@@ -68,7 +68,7 @@ function positionformfunc(i) {
     `;
 
     const position2 = `
-        <div id="errorform2_4">ff</div>
+        <div id="errorform${i}_4">ff</div>
         <div class="blockform">
             <input pattern="[A-N]{1}" class="col-3 block position" id="block_r${i}"  placeholder="英字1文字"  oninput="inputChange(${i})">
                 <b style="font-size:1.5rem">ブロック</b>      
