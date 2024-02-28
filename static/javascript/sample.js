d0e92585f049b0686075b9621bd3e1de1ef995aa
usamaru123 <usam260@gmail.com>
2024-02-28 23:56:42 +0900

    ff

--- static/javascript/sample.js
+++ static/javascript/sample.js
@@ -55,7 +55,6 @@ function positionformfunc(i) {
     const numberform = document.querySelector(`#numberform${i}`);
 
     const position1 = `
-        <div id="errorform1_4"></div>
         <div class="blockform">
             <input pattern="[A-Ga-g]{1}" class="col-3 block position" id="block_r${i}" placeholder="英字1文字"  oninput="inputChange(${i})">
             <input type="number" min="1" max="9" class="col-3 number position" id="block_c${i}" name="block_c${i}" placeholder="半角数字">
@@ -66,6 +65,7 @@ function positionformfunc(i) {
             placeholder="半角数字">
         <b style="font-size:1.5rem">番</b>
         </div>
+        <div id="errorform1_4"></div>
     `;
 
     const position2 = `
