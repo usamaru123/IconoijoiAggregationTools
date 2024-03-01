cb302120951a12fdfc1c7432ea47d4a3b15ac1c0
usamaru123 <usam260@gmail.com>
2024-03-01 18:48:24 +0900

    ff

--- jsapp/graph.py
+++ jsapp/graph.py
@@ -145,8 +145,7 @@ def Arena_HeatMap(rows,columns,sheets):
         colorscale='Edge',   
         zmax = 2,
         zmin = -2,
-        text=text,  # 追加するテキスト
-        texttemplate="%{text}",  # ホバーに追加する文字
+
         textfont={"size": 20}
         ),row=1,col=1)
     
