d54033216c598035b98364a07aa3887b11b0b7ca
usamaru123 <usam260@gmail.com>
2024-03-02 09:03:31 +0900

    gg

--- jsapp/views.py
+++ jsapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView, DeleteView, UpdateView,View
+from django.views.generic import ListView, DetailView, CreateView,UpdateView,View
 from .models import MenberModel,EventModel,VenueModel
 from django.urls import reverse_lazy
 from django.http import HttpResponse
