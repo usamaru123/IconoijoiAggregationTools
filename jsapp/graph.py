7933e98615aa37da748338137253547b3a04904f
usamaru123 <usam260@gmail.com>
2024-03-02 10:32:46 +0900

    っｆ

--- jsapp/graph.py
+++ jsapp/graph.py
@@ -196,7 +196,7 @@ def Arena_HeatMap(rows,columns,sheets):
     fig.update_layout(
         height=1500,
         margin_l=0,
-        margin_r=0,
+        margin_r=ｘ,
         font_family='sans-seif',
     )
     fig.update_traces(showscale=False)
