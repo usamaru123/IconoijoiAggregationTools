7cf06fbe70fa8c65c1808903f4248bdfbddbab20
usamaru123 <usam260@gmail.com>
2024-04-28 21:15:12 +0900

    ff

--- jsapp/models.py
+++ jsapp/models.py
@@ -31,13 +31,14 @@ class SheetModel(models.Model): #座席の属性を保存するマスタです
    def __str__(self):
       return str(self.priority) + "." + self.sheetname + ": " + self.prename +  self.sheettype.valname +  self.postname 
    
-class HallTypeDtlModel(models.Model):
-   typeid = models.IntegerField(default=1,primary_key=True)
-   typename = models.CharField(max_length=10)
+#class HallTypeDtlModel(models.Model):
+  # typeid = models.IntegerField(default=1,primary_key=True)
+  # typename = models.CharField(max_length=10)
 
 class HallTypeModel(models.Model): #会場の属性を保存するマスタです
    priority = models.IntegerField(default = 1)
    blockname = models.CharField(max_length=100)
+   halltype = models.CharField(max_length=100)
    sheets = models.ManyToManyField(SheetModel,default=[1])
    def __str__(self):
       return  self.blockname + "." + self.halltype
