61f6952c1b89ca3583e9cbecff1a592af7b5b839
usamaru123 <usam260@gmail.com>
2024-06-02 14:21:53 +0900

    ff

--- jsapp/models.py
+++ jsapp/models.py
@@ -95,8 +95,7 @@ class VenueModel(models.Model): #公演の情報を保存するフィールド�
    perform_time = models.ManyToManyField(m_PerformTime,default="")
    tickettype = models.ManyToManyField(TicketTypeModel,default="")
    sheettype = models.ManyToManyField(TicketSheetMaster,default="")
-   hallset = models.IntegerField(HallSetModel,on_delete=models.CASCADE,default=1)
-   
+   hallset = models.ForeignKey(HallSetModel,on_delete=models.CASCADE,default=1)
 
    def __str__(self):
       return str(self.venueid) + "." + self.event.eventtitle + self.hallinfo.hallname 
