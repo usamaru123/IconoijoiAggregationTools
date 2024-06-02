69fb57798d19ade0c7d051ce226d517d582267f7
usamaru123 <usam260@gmail.com>
2024-06-02 14:24:52 +0900

    ff

--- jsapp/models.py
+++ jsapp/models.py
@@ -62,7 +62,7 @@ class HallTypeModel(models.Model): #会場の属性を保存するマスタで�
    
 class HallSetModel(models.Model):
    setid = models.IntegerField(default=1)
-   setname = models.CharField(max_length=10)
+   setname = models.CharField(max_length=10,blank=True)
    hallset = models.ManyToManyField(HallTypeModel,default=[1])
 
 class HallInfoModel(models.Model): #会場の情報を保存するマスタです
