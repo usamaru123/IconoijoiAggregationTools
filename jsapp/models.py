da115492fe9446f815b872c3de6ef049cf38b7b2
usamaru123 <usam260@gmail.com>
2024-06-02 21:55:33 +0900

    ff

--- jsapp/models.py
+++ jsapp/models.py
@@ -66,17 +66,17 @@ class FloorModel(models.Model):
    def __str__(self):
       return str(self.priority) +"."+self.floorname
    
-class HallSetModel(models.Model):
-  setname = models.CharField(max_length=30,blank=True)
-  hallset = models.ManyToManyField(HallTypeModel,default="")
-  def __str__(self):
-     return self.setname
+#class HallSetModel(models.Model):
+ # setname = models.CharField(max_length=30,blank=True)
+  #hallset = models.ManyToManyField(HallTypeModel,default="")
+  #def __str__(self):
+   #  return self.setname
   
-class FloorSetModel(models.Model):
-  setname = models.CharField(max_length=30,blank=True)
-  hallset = models.ManyToManyField(FloorModel,default="")
-  def __str__(self):
-     return self.setname
+#class FloorSetModel(models.Model):
+ # setname = models.CharField(max_length=30,blank=True)
+  #hallset = models.ManyToManyField(FloorModel,default="")
+  #def __str__(self):
+   #  return self.setname
 
 
 class HallInfoModel(models.Model): #会場の情報を保存するマスタです
