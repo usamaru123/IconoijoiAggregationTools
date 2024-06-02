4aa2fe7995f1c51eb6f8db35f4bf55411d69019b
usamaru123 <usam260@gmail.com>
2024-06-02 17:59:56 +0900

    ffff

--- jsapp/admin.py
+++ jsapp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import MenberModel,EventModel,VenueModel,HallInfoModel,HallTypeModel,m_PerformTime,SheetModel,SheetValMaster,GroupModel,HallTypeDtlModel,EventTypeModel,TicketTypeModel,TicketSheetMaster
+from .models import MenberModel,EventModel,VenueModel,HallInfoModel,HallTypeModel,m_PerformTime,SheetModel,SheetValMaster,GroupModel,HallTypeDtlModel,EventTypeModel,TicketTypeModel,TicketSheetMaster,HallSetModel
 # Register your models here.
 admin.site.register(MenberModel)
 admin.site.register(EventModel)
@@ -14,3 +14,4 @@ admin.site.register(HallTypeDtlModel)
 admin.site.register(EventTypeModel)
 admin.site.register(TicketTypeModel)
 admin.site.register(TicketSheetMaster)
+admin.site.register(HallSetModel)
