4d431c2cffda093464541d036381944c7872fe18
usamaru123 <usam260@gmail.com>
2024-08-31 20:52:29 +0900

    ff

--- static/javascript/create2.js
+++ static/javascript/create2.js
@@ -59,7 +59,7 @@ function changeTicketSelect($this, time) {
             `<div class="form-check ${sale}">
                     <label for="${id}" class="form-check-label">
                         <input class="form-check-input ${formname}" type="${type}" name="${formname}"
-                    id="${id}" value="${ticketval}" onclick="changeSheetSelect(${sale},this,${time})">
+                    id="${id}" value="${ticketval}" onclick="changeSheetSelect('${sale}',this,${time})">
                     ${ticketval}
                 </label>
             </div>`;
