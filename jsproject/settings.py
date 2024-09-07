4cd4add7da68210211f0f68be57bdd2936957e4e
usamaru123 <usam260@gmail.com>
2024-09-07 11:27:14 +0900

    ff

--- jsproject/settings.py
+++ jsproject/settings.py
@@ -145,38 +145,3 @@ EMAIL_HOST_PASSWORD = '226600Usa'
 EMAIL_PORT = 587
 EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    # ログ出力フォーマットの設定
-    'formatters': {
-        'production': {
-            'format': '%(asctime)s [%(levelname)s] %(process)d %(thread)d '
-                      '%(pathname)s:%(lineno)d %(message)s'
-        },
-    },
-    # ハンドラの設定
-    'handlers': {
-        'file': {
-            'level': 'INFO',
-            'class': 'logging.FileHandler',
-            'filename': '/var/log/{}/app.log',
-            'formatter': 'production',
-        },
-    },
-    # ロガーの設定
-    'loggers': {
-        # 自分で追加したアプリケーション全般のログを拾うロガー
-        '': {
-            'handlers': ['file'],
-            'level': 'INFO',
-            'propagate': False,
-        },
-        # Django自身が出力するログ全般を拾うロガー
-        'django': {
-            'handlers': ['file'],
-            'level': 'INFO',
-            'propagate': False,
-        },
-    },
-}
