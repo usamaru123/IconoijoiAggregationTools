8175ef3c5a9ce0382222155ec238f0daa43a5bbf
usamaru123 <usam260@gmail.com>
2024-09-07 10:28:08 +0900

    ff

--- jsapp/apps.py
+++ jsapp/apps.py
@@ -5,6 +5,3 @@ class JsappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'jsapp'
 
-    def ready(self):
-        from .ap_scheduler import start
-        start()
