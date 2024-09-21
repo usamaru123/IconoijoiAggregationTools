7f0208e290fe4a37b6b69fece60fbab29ad587ff
usamaru123 <usam260@gmail.com>
2024-09-21 17:00:52 +0900

    ff

--- jsapp/views.py
+++ jsapp/views.py
@@ -186,7 +186,7 @@ class ContactCreate(CreateView):
     template_name = 'contact_create.html'
     models = t_answer
     queryset = t_answer.objects.all()
-    fields = ('__all__')
+    fields = ('nam','contact_text')
     success_url = reverse_lazy("list")
 
 class ContactThanks(TemplateView):
